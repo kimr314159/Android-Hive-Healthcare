a3eac5418b2bfc5e013677b9b81ac2705fe200b9
Kim D. Huynh <kimhuynh@gatech.edu>
2021-05-07 22:26:17 -0400

    defined private fields for MainActivity.

--- app/src/main/java/com/example/gileadproject/MainActivity.java
+++ app/src/main/java/com/example/gileadproject/MainActivity.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button buttonSend;
+    private EditText textMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+        textMessage = (EditText) findViewById(R.id.textMessage);
     }
 }
