ed3a5500cf5d3aa510e2862dc4459deab9754ac0
Kim Huynh <kimhuynh@gatech.edu>
2021-05-13 23:24:10 -0400

    save credentials as a private field.

--- app/src/main/java/com/example/gileadproject/MainActivity.java
+++ app/src/main/java/com/example/gileadproject/MainActivity.java
@@ -3,12 +3,12 @@ package com.example.gileadproject;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.dialogflow.v2.*;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonSend;
     private EditText textMessage;
     private static TextView textLog;
+    private GoogleCredentials credentials;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         buttonSend = (Button) findViewById(R.id.buttonSend);
         textMessage = (EditText) findViewById(R.id.textMessage);
         textLog = (TextView) findViewById(R.id.textLog);
+        setCredentials();
     }
 
     public void handleClick(View view){
@@ -31,25 +33,21 @@ public class MainActivity extends AppCompatActivity {
             case R.id.buttonSend:
                 //Store message before sending request
                 String message = textMessage.getText().toString();
-                textLog.append("\n" + message);
+                textLog.append(System.lineSeparator());
+                textLog.append(message);
                 System.out.println("Get Message: " + message);
                 System.out.println("Execute buttonSend Clicked");
                 textMessage.setText("");
-
         }
     }
 
-    private void getCredentials() {
+    private void setCredentials() {
         try{
             InputStream in = this.getResources().openRawResource(R.raw.credentials);
-            GoogleCredentials credentials = GoogleCredentials.fromStream(in);
+            credentials = GoogleCredentials.fromStream(in);
         }catch(Exception e){
-            e.printStackTrace();
-            System.out.println(e);
+            System.err.println("Failed to set credentials. " + e);
         }
     }
 
-    public void sendInputRequest(String message){
-        System.out.println("sendInputRequest is called.");
-    }
 }
